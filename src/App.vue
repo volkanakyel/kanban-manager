2be91deea401bd0a705cc1decb74892d0fd4ea1d
Volkan Akyel <volkan@uikielle.com>
2024-03-12 15:19:02 +0100

    kanban-manager | 🔧 Setup project tailwind with dark mode

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+const toggleDarkMode = () => {
+  document.body.classList.toggle("dark");
+}
+
+</script>
+
+<template>
+  <div class="bg-background text-text-primary">Hello
+    <button @click="toggleDarkMode" class=" mt-8 p-8">Dark mode</button>
+  </div>
+
+</template>
+
+<style scoped></style>
---
+++ tailwind.config.ts
@@ -0,0 +1,64 @@
+import { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      maxWidth: {
+        container: "940px",
+        background: "274px",
+        card: "138px",
+        addons: "450px",
+      },
+      width: {
+        addons: "450px",
+        container: "90%"
+      },
+      height: {
+        banner: "568px",
+      },
+      fontFamily: {
+        ubuntu: ["Ubuntu", "sans-serif"],
+      },
+    },
+    fontSize: {
+      xs: "12px",
+      base: "14px",
+      s: "16px",
+      sm: "18px",
+      m: "24px",
+      l: "32px",
+      lg: "40px",
+      xl: "56px",
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "940px",
+      xl: "1280px",
+    },
+    colors: {
+      background: 'var(--color-background)',
+      'text-primary': 'var(--color-text-primary)',
+      'text-secondary': 'var(--color-text-secondary)',
+      primary: 'var(--color-primary)',
+      secondary: 'var(--color-secondary)',
+      success: 'var(--color-success)',
+      info: 'var(--color-info)',
+      warning: 'var(--color-warning)',
+      highlight: 'var(--color-highlight)',
+      'dark-blue': 'var(--color-dark-blue)',
+      'light-blue': 'var(--color-light-blue)',
+      grey: {
+        100: 'var(--color-grey-100)',
+      },
+    },
+  },
+  darkMode: 'class',
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
